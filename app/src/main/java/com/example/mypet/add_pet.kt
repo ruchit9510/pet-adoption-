b2b41738f6122c2f88f0805835551095db873198
ruchit9510 <ruchitkapuriya9510@gmail.com>
2024-03-04 18:02:05 +0530

    Some Update

---
+++ app/src/main/java/com/example/mypet/add_pet.kt
@@ -0,0 +1,11 @@
+package com.example.mypet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_pet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pet)
+    }
+}
